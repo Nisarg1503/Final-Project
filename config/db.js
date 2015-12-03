fb9b90c182902f3d4e13a7b4ef2b4318b55dc2d1
Nisarg Patel <Nisarg.Patel@MyGeorgian.ca>
2015-12-03 13:49:02 -0500

    change home

--- config/db.js
+++ config/db.js
@@ -2,6 +2,6 @@ module.exports = {
 
 //'url':'mongodb://localhost/Auth'
 
-'url': 'mongodb://auth:port2@ds054298.mongolab.com:54298/auth'
+'url': 'mongodb://jnsurvey:jnpatel@ds054308.mongolab.com:54308/jnsurvey'
 
 };
